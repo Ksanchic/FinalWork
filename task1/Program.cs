6d8ac8dda6e1f7306c361b196ad8a76eb91a2cb8
ksanchic <ksanchic@mail.ru>
2022-12-26 18:08:48 +0300

    Task с вводом элементов массива с клавиатуры

---
+++ task1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+
+string[] array = NewArray();
+string[] result = FindLessThan(array, 3);
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] FindLessThan(string[] input, int n) 
+{
+    string[] output = new string[CountLessThan(input, n)];
+    for(int i = 0, j = 0; i < 3; i++) 
+    {
+        if(input[i].Length <= n)
+         {
+            output[j] = input[i];
+            j++;
+        }
+    }
+    return output;
+}
+
+int CountLessThan(string[] input, int n) 
+{
+    int count = 0;
+    for(int i = 0; i < 3; i++) 
+    {
+      count++;
+    }
+    return count;
+}
+string[] NewArray() 
+{
+    Console.Write("Введите значения через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
